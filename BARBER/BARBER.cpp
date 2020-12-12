e7c59a435dedb65693c17b7081643ecb79f7fd31
Logvinov Dmitry <logvinovdmitriv@gmail.com>
2020-12-12 19:34:49 +0300

    Добавьте файлы проекта.

---
+++ BARBER/BARBER.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>      
+#include <thread>        
+#include <mutex>         
+#include <condition_variable>
+#include <list>
+#include <semaphore>
+#include  <chrono>
+
+std::mutex mtx;             // mutex for critical section
+std::condition_variable cv; // condition variable for critical section  
+bool ready = false;         // Tell threads to run
+int current = 0;            // current count
+std::thread threads[15];
+
+/* Prints the thread id / max number of threads */
+void DoBarberWork(int num) {
+
+    std::unique_lock<std::mutex> lck(mtx);
+   
+    while (num != current || !ready) 
+    {
+        if (!ready)
+        {
+            std::cout << "Customer NO " << num << " came, BARBERSHOP CLOSED\n";
+        };
+        if (num != current && ready) 
+        {
+            std::cout << "Customer NO " << num << " Waiting\n";
+        };
+        cv.wait(lck);
+    }
+    current++;
+    std::cout << "Barber Starts\t";
+    std::this_thread::sleep_for(std::chrono::milliseconds(rand()%500 + 500));
+    std::cout << "current NO of Customer is: " << current << "\t";;
+    std::cout << "Barber Ends\n\n";
+
+    /* Notify next threads to check if it is their turn */
+    cv.notify_all();
+}
+
+/* Changes ready to true, and begins the threads printing */
+void runBarberShop() {
+    std::unique_lock<std::mutex> lck(mtx);
+    ready = true;
+    cv.notify_all();
+}
+
+int main() {
+
+    int threadnum = 15;
+ 
+
+    /* Создание потоков ПОСЕТИТЕОЕЙ */
+    for (int id = 0; id < threadnum; id++)
+    {
+        std::cout << "Customer NO " << id << " CREATED\n";
+        threads[id] = std::thread(DoBarberWork, id);
+        std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 500));
+    }
+
+   
+    std::cout << "BARBERSHOP OPEN\n";
+    runBarberShop(); // ОТКРЫТИЕ ПАРИКМАРЕРСКОЙ
+
+    /* Merge all threads to the main thread */
+    for (int id = 0; id < threadnum; id++)
+        threads[id].join();
+
+    std::cout << std::endl;
+
+    return 0;
+}
